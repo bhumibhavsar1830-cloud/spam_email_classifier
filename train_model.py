eed59ac53192c423c42b73abcd0f913d5ea40205
bhumibhavsar1830-cloud <bhumibhavsar1830@gmail.com>
2026-03-09 07:35:39 -0700

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -1,1 +1,40 @@
+import pandas as pd
+import pickle
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score
 
+# Load dataset
+data = pd.read_csv("data/spam.csv")
+
+# Rename columns
+data.columns = ["label", "message"]
+
+# Convert labels
+data["label"] = data["label"].map({"ham":0, "spam":1})
+
+# Features and target
+X = data["message"]
+y = data["label"]
+
+# Vectorization
+vectorizer = TfidfVectorizer(stop_words="english")
+X = vectorizer.fit_transform(X)
+
+# Split dataset
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+# Train model
+model = MultinomialNB()
+model.fit(X_train, y_train)
+
+# Prediction
+pred = model.predict(X_test)
+
+print("Accuracy:", accuracy_score(y_test, pred))
+
+# Save model
+pickle.dump((model, vectorizer), open("model/spam_model.pkl", "wb"))
